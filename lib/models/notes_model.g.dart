70166ccc0f7c2c54a53689e9235c124eb8ca74c3
saeedshaikh29 <saeedshaikh2921@gmail.com>
2026-05-22 13:53:47 +0530

    accidental not commmit

---
+++ lib/models/notes_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notes_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      pageNumber: fields[0] as int,
+      memorizationNotes: fields[1] as String,
+      testModeNotes: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.pageNumber)
+      ..writeByte(1)
+      ..write(obj.memorizationNotes)
+      ..writeByte(2)
+      ..write(obj.testModeNotes);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
